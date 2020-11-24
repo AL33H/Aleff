0a29b73a9fe1282c2c7d295a684bccb8b21b858b
al33 <aleff.remberto@hotmail.com>
2020-11-24 18:27:18 -0300

    Meu segundo commit

--- src/agora/Agora.java
+++ src/agora/Agora.java
@@ -1,17 +1,12 @@
 
 package agora;
 
-/**
- *
- * @author ALEFF
- */
+
 public class Agora {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
